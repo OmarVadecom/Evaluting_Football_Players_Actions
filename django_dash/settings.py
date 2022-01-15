4d79830fcb32642f7318f5a4a9012c39f88fd9d3
Omar Wael <omar_wael1890@yahoo.com>
2022-01-15 13:44:40 +0200

    Update settings.py

--- django_dash/settings.py
+++ django_dash/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-()9d3=dizn@w=v*va+(p#-lnb@%u6$hlzi3!l@w^(-vl@$86*_'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
